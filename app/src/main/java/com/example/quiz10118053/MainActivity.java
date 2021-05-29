b54d67233983bf2a4996c210bd217d32aae9c741
kevinalvian <kevinalvian166@gmail.com>
2021-05-29 10:51:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118053/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.quiz10118053;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//<10118053>
+//<Kevin Alvian>
+//<IF-2>
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
